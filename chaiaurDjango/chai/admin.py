12181512009582d07cc39eb4523cb30b1fdc45f8
nikhil <nikhilsharma270027@gmail.com>
2025-06-14 00:47:54 +0530

    add admin work flow

--- chaiaurDjango/chai/admin.py
+++ chaiaurDjango/chai/admin.py
@@ -1,4 +1,30 @@
 from django.contrib import admin
-from .models import ChaiVarity
+from .models import ChaiVarity, ChaiReview, Store, ChaiCertificate
 # Register your models here.
-admin.site.register(ChaiVarity)
+
+# 
+class ChaiReviewInline(admin.TabularInline): 
+    # TabularInline displays the form in a table format
+    # # StackedInline displays the form in a stacked format
+    model = ChaiReview
+    extra = 2 # Number of empty forms to display
+    # extra 2 means how many empty forms to display in the inline formset
+
+class ChaiVarityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')  # Fields to display in the list view
+    # search_fields = ('name', 'type')  # Fields to search in the admin interface/
+    # list_filter = ('type',)  # Fields to filter by in the admin interface
+    inlines = [ChaiReviewInline]  # Inline model for reviews
+    
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')  # Fields to display in the list view
+    # search_fields = ('name', 'location')  # Fields to search in the admin interface
+    filter_horizontal = ('chai_varities',)  # Many-to-many field for chai varities
+    
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')  # Fields to display in the list view
+    # search_fields = ('chai__name', 'certificate_number')  # Fields to search in the admin interface    
+    
+admin.site.register(ChaiVarity, ChaiVarityAdmin) # injecting the ChaiVarityAdmin class
+admin.site.register(Store, StoreAdmin) # injecting the StoreAdmin class
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin) # injecting the ChaiCertificateAdmin class
--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+# bydefault django has user model
+from django.contrib.auth.models import User
 # Create your models here.
 class ChaiVarity(models.Model):
     CHAI_TYPE_CHOICE = [
@@ -17,6 +19,40 @@ class ChaiVarity(models.Model):
     description = models.TextField(default="")
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     
+    # this is called a dander method
+    def __str__(self): 
+        return self.name
+    # what does the above line do?
+    # it returns the name of the chai when we print the object
+    
+    
+#one to many relationship
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVarity, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    comment = models.TextField(default="")
+    data_added = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f'{self.user.username} reviws for {self.chai.name}'
+    
+    
+    # Many to many relationship
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+    chai_varities = models.ManyToManyField(ChaiVarity, related_name='stores') # store ke naam se relation
+    
     def __str__(self):
         return self.name
     
+#  One to one relationship
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVarity, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=50, unique=True)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField()
+    
+    def __str__(self):
+        return f'Certificate for {self.chai.name}'
